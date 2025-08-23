732c9dec65c2244c0689f68d55196300635a3762
danny444-ops <kwartengdaniel432@gmail.com>
2025-08-23 23:17:23 +0000

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     // ===== BOT CORE SETTINGS =====
 
-    SESSION_ID: process.env.SESSION_ID || "MEGALODON-MD~",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~YQxknZTS#0ikiHUlVz6lRKKCvjV3kgOo81MKypbeQrpraXqGvVNA",  // Your bot's session ID (keep it secure)
     PREFIX: process.env.PREFIX || ".",  // Command prefix (e.g., "., / ! * - +")
     BOT_NAME: process.env.BOT_NAME || "MEGALODON-MD",  // Bot's display name
     MODE: process.env.MODE || "public",        // Bot mode: public/private/group/inbox
@@ -72,6 +72,6 @@ module.exports = {
     
     WELCOME: process.env.WELCOME || "false",
 
-    GOODBYE: process.env.GOODBYE || "true",
+    GOODBYE: process.env.GOODBYE || "false",
    
 };
